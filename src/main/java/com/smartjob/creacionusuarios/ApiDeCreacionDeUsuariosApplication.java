2a37bfa28c2a52fdb72e3b8323792a0623db14a3
oskarina gonzalez <oskarinags@gmail.com>
2025-05-24 19:33:48 -0400

    First commit

---
+++ src/main/java/com/smartjob/creacionusuarios/ApiDeCreacionDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.smartjob.creacionusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeCreacionDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeCreacionDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartjob/creacionusuarios/ApiDeCreacionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartjob.creacionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeCreacionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
