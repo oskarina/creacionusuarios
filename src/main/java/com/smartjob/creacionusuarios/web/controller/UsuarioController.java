bbb1e8f3fff907edd63be11970bfe8e40f6e21c6
oskarina gonzalez <oskarinags@gmail.com>
2025-05-24 20:21:59 -0400

    Test UsuarioController - green - 200OK

--- src/main/java/com/smartjob/creacionusuarios/web/controller/UsuarioController.java
+++ src/main/java/com/smartjob/creacionusuarios/web/controller/UsuarioController.java
@@ -1,4 +1,12 @@
 package com.smartjob.creacionusuarios.web.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+    @PostMapping("/usuarios")
+    public String crearUsuario(){
+        return "Usuario creado!";
+    }
 }
--- src/test/java/com/smartjob/creacionusuarios/web/controller/UsuarioControllerTest.java
+++ src/test/java/com/smartjob/creacionusuarios/web/controller/UsuarioControllerTest.java
@@ -1,16 +1,20 @@
 package com.smartjob.creacionusuarios.web.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UsuarioController.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class UsuarioControllerTest {
 
     @Autowired
@@ -18,9 +22,17 @@ public class UsuarioControllerTest {
 
     @Test
     void testCreacion() throws Exception {
-        mockMvc.perform(post("/api/usuarios/"))
+        mockMvc.perform(post("/usuarios"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Usuario creado!"));
+    }
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void printMappings() {
+        System.out.println(webApplicationContext.getBean(RequestMappingHandlerMapping.class).getHandlerMethods());
     }
 
 }
