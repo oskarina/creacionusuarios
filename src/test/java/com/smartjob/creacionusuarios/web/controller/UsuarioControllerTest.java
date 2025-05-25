685647efa51b69b0a770cb1355e8b1aa2a42d246
oskarina gonzalez <oskarinags@gmail.com>
2025-05-24 20:43:09 -0400

    Test UsuarioController - red - espero DTO

--- src/test/java/com/smartjob/creacionusuarios/web/controller/UsuarioControllerTest.java
+++ src/test/java/com/smartjob/creacionusuarios/web/controller/UsuarioControllerTest.java
@@ -9,8 +9,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -22,9 +25,18 @@ public class UsuarioControllerTest {
 
     @Test
     void testCreacion() throws Exception {
+        String dateString = "2025-05-24 20:30:00";
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date created = formatter.parse(dateString);
+
         mockMvc.perform(post("/usuarios"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Usuario creado!"));
+                .andExpect(jsonPath("$.id").value("Oskarina"))
+                .andExpect(jsonPath("$.created").value(created))
+                .andExpect(jsonPath("$.modified").value(created))
+                .andExpect(jsonPath("$.lastLogin").value(created))
+                .andExpect(jsonPath("$.token").value("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"))
+                .andExpect(jsonPath("$.isActive").value(true));
     }
 
     @Autowired
